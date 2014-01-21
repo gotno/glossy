1d5234b71006f2837f693d7c5a33584f2cd30d2f
gotno <floyd.will@gmail.com>
2014-01-20 17:58:09 -0800

    add text widget link

--- app/assets/javascripts/views/sections_edit.js
+++ app/assets/javascripts/views/sections_edit.js
@@ -3,12 +3,57 @@ Glossy.Views.SectionsEdit = Backbone.View.extend({
 
   tagName: 'section',
 
+  events: {
+    'click a.section-add-text': 'addTextWidget',
+    'click a.seciton-add-image': 'addImageWidget'
+  },
+
+  initialize: function() {
+    this.widgetOrder = 0;
+    this.widgetViews = [];
+
+    if (!this.model.get('textWidgets')) {
+      this.model.set('textWidgets', new Glossy.Collections.TextWidgets());
+    }
+    this.listenTo(this.model.get('textWidgets'), 'add', this.renderTextWidget);
+
+    if (!this.model.get('imageWidgets')) {
+      this.model.set('imageWidgets', new Glossy.Collections.ImageWidgets());
+    }
+    this.listenTo(this.model.get('imageWidgets'), 'add', this.renderImageWidget);
+  },
+
   render: function() {
     this.$el.html(this.template({
       section: this.model
     }));
+
     return this;
-  } ,
+  },
+
+  addTextWidget: function(event) {
+    event.preventDefault();
+
+    var textWidget = new Glossy.Models.TextWidget({
+      ord: this.widgetOrder
+    });
+
+    this.model.get('textWidgets').add(textWidget);
+  },
+
+  renderTextWidget: function() {
+    var newTextWidgetView = new Glossy.Views.TextWidget({
+      model: this.model.get('textWidgets').last()
+    });
+
+    this.widgetViews.push(newTextWidgetView);
+
+    this.$el.append(newTextWidgetView.render().$el);
+  },
+
+  addImageWidget: function(event) {
+    event.preventDefault();
+  },
 
   collect: function() {
     var ord = this.model.get('ord');
--- app/assets/javascripts/views/widgets/text.js
+++ app/assets/javascripts/views/widgets/text.js
@@ -1,5 +1,5 @@
 Glossy.Views.TextWidget = Backbone.View.extend({
-  template: JST['widgets/text'],
+  template: JST['widgets/text_new'],
 
   render: function() {
     this.$el.html(this.template({
