5c4d58d96e32af0ee1e8adf9bc3b38f246eaa674
gotno <floyd.will@gmail.com>
2014-02-08 13:49:27 -0800

    update views for row addition

--- db/migrate/20140126233505_create_rows.rb
+++ db/migrate/20140126233505_create_rows.rb
@@ -1,8 +1,8 @@
 class CreateRows < ActiveRecord::Migration
   def change
     create_table :rows do |t|
-      t.integer :section_id
-      t.integer :ord
+      t.integer :section_id, null: false
+      t.integer :ord, null: false
 
       t.timestamps
     end
